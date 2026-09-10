97110ce1ade50d96a61c078cd9927d79d168646d
Arup <arupkumarmidya100@gmail.com>
2026-09-10 20:54:14 +0530

    Arithmetic OPeration in Numpy array

---
+++ Arithmetic Operation in Numpy.py
@@ -0,0 +1,69 @@
+# Addition Operation:-
+import numpy as np
+val1=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+val2=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(val1)
+print(val2)
+print(val1+val2)
+print(np.add(val1,val2))
+
+
+#Subtraction Operation:-
+print()
+print()
+import numpy as np
+val1=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+val2=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(val1)
+print(val2)
+print(np.subtract(val1,val2))
+
+# Multiply Opration:-
+print()
+print()
+import numpy as np
+val1=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+val2=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(val1)
+print(val2)
+print(np.multiply(val1,val2))
+
+#Division Operation:-
+print()
+print()
+import numpy as np
+val1=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+val2=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(val1)
+print(val2)
+print(np.divide(val1,val2))
+
+#Modulo Operation:-
+print()
+print()
+import numpy as np
+val1=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+val2=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(val1)
+print(val2)
+print(np.mod(val1,val2))
+
+#Power Operation:-
+print()
+print()
+import numpy as np
+val1=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+val2=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(val1)
+print(val2)
+print(np.power(val1,val2))
+
+#Receprocal Operation:-
+print()
+print()
+import numpy as np
+val1=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+val2=np.array([[1,2,3,4,5],[6,7,8,9,0]])
+print(val1)
+print(val2)
+print(np.reciprocal(val1,val2))
